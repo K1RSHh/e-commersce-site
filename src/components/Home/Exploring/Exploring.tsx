f1070b760d87abc63f3b8e352a3d42d14eaf1d86
K1RSHh <mark2jzx100ua@gmail.com>
2026-02-28 09:10:42 +0200

    fix

--- src/components/Home/Exploring/Exploring.tsx
+++ src/components/Home/Exploring/Exploring.tsx
@@ -1,6 +1,10 @@
 function Exploring() {
+  const bgImageUrl = "/home/TECH-GAGDETS.jpg";
   return (
-    <div className="relative h-130 max-w-312.5 flex rounded-2xl bg-no-repeat bg-center bg-cover bg-[url('home/TECH-GAGDETS.jpg')]">
+    <div
+      className="relative h-130 max-w-312.5 flex rounded-2xl bg-no-repeat bg-center bg-cover"
+      style={{ backgroundImage: `url(${bgImageUrl})` }}
+    >
       <div className="absolute right-0 bottom-10">
         <div className="text-right mr-10">
           <p className="text-6xl md:text-7xl font-bold mb-8">Exploring</p>
